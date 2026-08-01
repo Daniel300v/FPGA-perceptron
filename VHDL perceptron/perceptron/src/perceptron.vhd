612cdd5379fa85fd46e63abee342334631596929
Daniel Hogg <dan300vgames@gmail.com>
2026-08-01 12:18:05 +0100

    added the ability to update values. UNTESTED

--- VHDL perceptron/perceptron/src/perceptron.vhd
+++ VHDL perceptron/perceptron/src/perceptron.vhd
@@ -3,6 +3,7 @@ use IEEE.std_logic_1164.all;
 
 entity perceptron is
     port(source : in std_logic_vector(7 downto 0);
+         selector  : in std_logic_vector (1 downto 0);
          sink : out std_logic);
 end perceptron;
 
@@ -29,22 +30,40 @@ architecture perceptronLogic of perceptron is
     signal carry : std_logic_vector (7 downto 0);
     signal neuronProduct : std_logic_vector (7 downto 0);
     
-
+    signal weight : std_logic_vector(2 downto 0) := "010";
+    signal bias : std_logic_vector(7 downto 0) := "00000101";
+    signal threshold : std_logic_vector(7 downto 0) := "00010000";
     begin
+
+        
         start: barrelShifter
         port map (barrelIn => source,
-             totalShift => "010",
+             totalShift => weight,
              barrelOut => carry);
 
         offset: eightBitAdder
         port map (inA => carry,
-             inB => "00000101",
+             inB => bias,
              SUM => neuronProduct);
-        
+            
         comp: comparitor
         port map (perceved => neuronProduct,
-                  static => "00010000",
+                  static => threshold,
                   activation => sink);
-        
+    process(selector, source)
+    begin
+        case selector is
+            when "00" =>
+                null;
+            when "01" =>
+                weight <= source(2 downto 0); 
+            when "10" =>
+                bias <= source;
+            when "11" =>
+                threshold <= source;
+            when others =>
+                null;
+        end case;
+    end process;
 
 end perceptronLogic;
